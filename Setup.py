7d041bc7411c720a8f132a379143dfbb26e562bd
AJ Auntor <auntorknocks@gmail.com>
2021-12-10 21:18:37 +0600

    py script name

--- Setup.py
+++ Setup.py
@@ -29,3 +29,18 @@ def read_requirements(path):
     ]
 
 
+setup(
+    name="project_name",
+    version=read("project_name", "VERSION"),
+    description="project_description",
+    url="https://github.com/author_name/project_urlname/",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="author_name",
+    packages=find_packages(exclude=["tests", ".github"]),
+    install_requires=read_requirements("requirements.txt"),
+    entry_points={
+        "console_scripts": ["project_name = project_name.__main__:main"]
+    },
+    
+)
