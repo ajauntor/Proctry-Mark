f1ffb05fcbdf29046f65d3169f44f6ab54da067b
AJ Auntor <auntorknocks@gmail.com>
2021-12-10 21:08:03 +0600

    Flag start

---
+++ Medad.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+while getopts a:n:u:d: flag
+do
+    case "${flag}" in
+        a) author=${OPTARG};;
+        n) name=${OPTARG};;
+        u) urlname=${OPTARG};;
+        d) description=${OPTARG};;
+    esac
+done
+
