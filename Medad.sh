a47321031fb401a39cb692b46e472a7617a57572
AJ Auntor <auntorknocks@gmail.com>
2021-12-20 22:01:30 +0600

    template_DIR

--- Medad.sh
+++ Medad.sh
@@ -50,11 +50,7 @@ rm -rf tests
 cp -R ${TEMPLATE_DIR}/project_name "${name}"
 cp -R ${TEMPLATE_DIR}/tests tests
 
-cp ${TEMPLATE_DIR}/README.md README.md
-cp ${TEMPLATE_DIR}/Containerfile Containerfile
-cp ${TEMPLATE_DIR}/wsgi.py wsgi.py
-cp ${TEMPLATE_DIR}/.env .env
-cp ${TEMPLATE_DIR}/settings.toml settings.toml
+
 
 # install
 make clean
